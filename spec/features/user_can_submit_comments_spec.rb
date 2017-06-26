f82fa53d460d2c9a508ca93e099ccbd3d5795526
Colin Scally <colin.scally@gmail.com>
2017-06-26 17:07:04 +0100

    Feature complete - add comments to posts

---
+++ spec/features/user_can_submit_comments_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Commenting", type: :feature do
+
+  scenario "Can submit comments on existing posts and view them" do
+    Post.create(message: "Test Post")
+    visit '/posts'
+    click_link('Show')
+    fill_in "Commenter", with: "Rita"
+    fill_in "Body", with: "I approve this message! :thumbsup:"
+    click_button "Create Comment"
+    expect(page).to have_content('I approve this message!')
+  end
+
+end
