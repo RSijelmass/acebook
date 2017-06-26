86b7e96da0c637198f226b871afb642074c8d18d
RSijelmass <R.Sijelmass@gmail.com>
2017-06-26 16:42:00 +0100

    Creates unit test model comments -takes in commenter

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-	let(:comment) { Comment.create(body: "The internet is so lame. Rage quit!") }
+	let(:comment) { Comment.create(body: "The internet is so lame. Rage quit!", commenter: "ColinisCool") }
 
 	describe 'creating a comment' do 
 		it 'should have a body' do
 			expect(comment.body).to eq "The internet is so lame. Rage quit!"
 		end
-	end	
+		
+		it 'should have a commenter' do
+			expect(comment.commenter).to eq "ColinisCool"
+		end
+end	
 end
