2bf230b4e0b5cf472e60500629ee8a36625706b0
Colin Scally <colin.scally@gmail.com>
2017-06-26 16:11:35 +0100

    Implement MVC for comments - Add Comment controller - Create link between comments and posts - View to disply form to make comment §

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,12 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
@@ -6,10 +14,6 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(post_params)
     redirect_to posts_url
-  end
-
-  def index
-    @posts = Post.all
   end
 
   private
