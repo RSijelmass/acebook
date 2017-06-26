6eb0b6f5b70d31aab2318f5e125ac5f4d232f66a
RSijelmass <R.Sijelmass@gmail.com>
2017-06-26 16:40:03 +0100

    Creates unit test for model comments - takes in a body

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:comment) { Comment.create(body: "The internet is so lame. Rage quit!") }
+
+	describe 'creating a comment' do 
+		it 'should have a body' do
+			expect(comment.body).to eq "The internet is so lame. Rage quit!"
+		end
+	end	
 end
