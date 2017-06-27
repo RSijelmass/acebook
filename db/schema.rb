7fd02b08cdfa11416f9567298c84c053f95c3abd
RSijelmass <R.Sijelmass@gmail.com>
2017-06-27 12:11:32 +0100

    Adds 'likes' variable to Post schema - default value of 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626140715) do
+ActiveRecord::Schema.define(version: 20170627110756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170626140715) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes", default: 0
   end
 
   add_foreign_key "comments", "posts"
