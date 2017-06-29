e057ffc9f322943e8d3243a870d3e14f2598ee4c
“rogrenke” <roland@rolandgrenke.com>
2017-06-29 12:45:59 +0100

    Adds functionality to flag posts - flags are only displayed if they are own flags - maximum flag per post and user is 1 - feature tested

--- spec/helpers/sign_up_helper_spec.rb
+++ spec/helpers/sign_up_helper_spec.rb
@@ -7,3 +7,12 @@ def sign_up
   fill_in('user_password', with: 'secret')
   click_button('Sign up')
 end
+
+def sign_up_other
+  visit '/'
+  click_button('Sign in')
+  click_link('Sign up')
+  fill_in('user_email', with: 'toby@curd.org')
+  fill_in('user_password', with: 'alsosecret')
+  click_button('Sign up')
+end
