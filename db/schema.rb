a8ae84411a8a685fac106fe4497ce96cd6d6042b
Colin Scally <colin.scally@gmail.com>
2017-06-29 16:48:55 +0100

    creates link between comments and users, gets rid of 'commenter' field in comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629091233) do
+ActiveRecord::Schema.define(version: 20170629151514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
     t.text "body"
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "flags", force: :cascade do |t|
@@ -64,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170629091233) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "flags", "posts"
   add_foreign_key "flags", "users"
   add_foreign_key "likes", "posts"
--- spec/features/user_can_submit_comments_spec.rb
+++ spec/features/user_can_submit_comments_spec.rb
@@ -6,7 +6,6 @@ RSpec.feature "Commenting", type: :feature do
     sign_up
 		create_post
     click_link('Show')
-    fill_in "Commenter", with: "Rita"
     fill_in "Body", with: "I approve this message! :thumbsup:"
     click_button "Create Comment"
     expect(page).to have_content('I approve this message!')
