f2529f26eed4ff288cfb3653c19a431fd329c34b
RSijelmass <R.Sijelmass@gmail.com>
2017-06-30 12:01:10 +0100

    Fixes test 'identify questionable links' - creates a source before testing feature

--- spec/features/identify_questionable_links_spec.rb
+++ spec/features/identify_questionable_links_spec.rb
@@ -1,9 +1,19 @@
 describe 'Questionable links' do
   let(:source) { FactoryGirl.build(:source) }
-  
+ 
+	before(:each) do
+    Source.create(url: "breitbart.com",
+    type_one: "Political",
+    type_two: "Unreliable",
+    type_three: "Bias",
+    notes: nil,
+    location: nil,
+    source: 'OpenSources')
+  end
+ 
   it 'are checked against the Source database and tagged in posts' do
     sign_up
     create_post('Check out this horrible site, itz grrrrr8 www.breitbart.com !')
-    expect(page).to have_content('Political')
+		expect(page).to have_content('Political')
   end
 end
