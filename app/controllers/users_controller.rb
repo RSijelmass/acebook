4d9cce11ef2138cdc0787cfcb96bf95bb9d669ad
Colin Scally <colin.scally@gmail.com>
2017-06-30 15:18:14 +0100

    creates a user profile which contains the posts belonging to the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < Clearance::UsersController
 
   def show
     @user = User.find(params[:id])
+    @userposts = Post.where(user_id: @user.id)
   end
 
 
--- spec/features/users_can_see_profiles_spec.rb
+++ spec/features/users_can_see_profiles_spec.rb
@@ -9,4 +9,10 @@ describe 'user can see profiles' do
       expect(page).to have_content('Colin')
     end
 
+		it ' - visit own profile' do
+			create_post('I should see this')
+			click_link('My Profile')
+			expect(page).to have_content('I should see this')
+    end
+
 end
